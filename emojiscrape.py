2075925a4426150f5f81081187289aa3d2d76b22
Chetan Padia <chetbox@users.noreply.github.com>
2016-07-05 22:06:03 +0100

    fetch and index some emojis (WIP)

---
+++ emojiscrape.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+from lxml import html
+import requests
+from os.path import isfile
+import re
+
+def fetch_emojis():
+    DATA_CACHE_FILE = 'data/full-emoji-list.html'
+
+    def http_get():
+        print('Fetching emojis')
+        return requests.get('http://unicode.org/emoji/charts/full-emoji-list.html').content
+
+    def is_cached():
+        return DATA_CACHE_FILE and isfile(DATA_CACHE_FILE)
+
+    def read_cached():
+        return open(DATA_CACHE_FILE, encoding='utf-8').read()
+
+    def write_cached(content):
+        if DATA_CACHE_FILE:
+            with open(DATA_CACHE_FILE, 'w', encoding='utf-8') as f:
+                f.write(content)
+
+    if is_cached():
+        page = read_cached()
+    else:
+        page = http_get()
+        write_cached(page)
+    return page
+
+def each_emoji(tree):
+    return tree.xpath('//div[@class="main"]/table/tr[td]')
+
+def emoji_info(tr):
+    def first(node):
+        return ''.join(node[:1])
+    def year(s):
+        return re.match(r'[0-9]{4}', s).group()
+    def code_to_emoji(code):
+        return re.sub(r'U\+([0-9A-Z]+) *', lambda m: chr(int(m.groups()[0], 16)), code)
+    return {
+        'index': int(first(tr.xpath('td[@class="rchars"]//text()'))) - 1,
+        'chars': code_to_emoji(first(tr.xpath('td[@class="code"]//text()'))),
+        'code': first(tr.xpath('td[@class="code"]//text()')),
+        'name': first(tr.xpath('td[@class="name"]/text()')),
+        'keywords': tr.xpath('td[@class="name"]/a[@target="annotate"]/text()'),
+        'age': year(first(tr.xpath('td[@class="age"]//text()')))
+    }
+
+def extract_emojis(emoji_html):
+    tree = html.fromstring(emoji_html)
+    return [emoji_info(e) for e in each_emoji(tree)]
+
+def all_keywords(emojis):
+    return [keyword for emoji in emojis if emoji for keyword in emoji.get('keywords', [])]
+
+def build_index(emojis):
+    index = {}
+    for emoji in emojis:
+        if not emoji:
+            continue
+        for keyword in emoji['keywords']:
+            keywords = index.get(keyword, {})
+            keywords[emoji['index']] = 0.5 
+            index[keyword] = keywords
+    return index
+
+def search(emojis, index, query):
+    results = index.get(query, {})
+    # results.sort(key=lambda p, _: p) # TODO: sort by prob
+    return [(p, emojis[index]) for (index, p) in results.items()]
+
+def build_trie(words, current_prefix=''):
+    trie = {}
+    if any([len(word) == 0 for word in words]):
+        trie[None] = current_prefix
+    for char in {word[0] for word in words if len(word) > 0}:
+        next_words = [word[1:] for word in words if len(word) > 0 and word[0] == char]
+        trie[char] = build_trie(next_words, current_prefix + char)
+    return trie
+
+def all_values(trie):
+    value = trie.get(None)
+    subtries = [subtrie for (char, subtrie) in trie.items() if char]
+    return ([value] if value else []) \
+        + [word for subtrie in subtries for word in all_values(subtrie)]
+
+def prefix_search(emojis, index, trie, query):
+    def find_node(trie, query):
+        if not query:
+            return trie
+        return trie and find_node(trie.get(query[0]), query[1:])
+    def keywords(trie, query):
+        node = find_node(trie, query)
+        return all_values(node) if node else []
+    return [result for keyword in keywords(trie, query) for result in search(emojis, index, keyword)]
+
+
+def emoji_description(emoji):
+    return emoji['chars'] + ' ' + emoji['name']
+
+def print_prefix_search(query):
+    print(query + '|')
+    for (p, result) in prefix_search(emojis, index, trie, query):
+        print('\t%0.3f\t%s' % (p, emoji_description(result))) 
+
+if __name__ == '__main__':
+    from pprint import pprint
+    emoji_html = fetch_emojis()
+    emojis = extract_emojis(emoji_html)
+    index = build_index(emojis)
+    trie = build_trie(all_keywords(emojis))
+
+    print_prefix_search('hal')
