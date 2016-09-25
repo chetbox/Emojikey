ce78f654efbc9bdcef721508bd6620c45a6008a7
Chetan Padia <chetbox@users.noreply.github.com>
2016-09-25 15:35:28 +0100

    💥 Overhaul: move UI into a popup
    + use Chrome shortcut keys
    + update on-boarding accodingly

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
+'use strict';
+
+// Set up search
 $.getJSON('/config_resources/emoji-data.json', db => {
   chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
-      let results = search(request.query, db);
-      sendResponse({results: results});
+      sendResponse({results: search(request.query, db)});
   });
 });
 
 let installedVersion = localStorage['version'];
+let currentVersion = chrome.app.getDetails().version;
+localStorage['version'] = currentVersion;
 
 // Open hello.html when extension installed
 if (!installedVersion) {
-  chrome.runtime.onInstalled.addListener(function (object) {
-    chrome.tabs.create({
-      url: chrome.extension.getURL('hello.html')
-    });
+  chrome.runtime.onInstalled.addListener(object => {
+    chrome.tabs.create({url: chrome.extension.getURL('hello.html')});
   });
 }
-
-localStorage['version'] = chrome.app.getDetails().version;
